aaf2fda3fe59d663f1a48c076e0a949422706d8a
nergui <nergui.badarch@gmail.com>
2014-11-20 21:42:02 -0600

    come branch

--- spiralPrinting.java
+++ spiralPrinting.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
+// some branche
 public class Main {
 
 	public static void main (String[] args) {
